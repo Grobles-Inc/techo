7f402b6630b25b9a74083c39ec2ac72288a6692e
kralion <joan300501@gmail.com>
2025-07-24 11:31:37 -0500

    Initial Setup with daysi ui, astro, tailwind and lucide

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tu Techo Ahora';
+export const SITE_DESCRIPTION = 'Negocios Inmobiliarios, Confianza, Seguridad y Eficiencia.';
