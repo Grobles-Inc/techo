a29e42994907ffc4822d34671c0473f65ba6ba0c
kralion <joan300501@gmail.com>
2025-07-26 09:54:25 -0500

    upload properties form and vercel adapter installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import vercel from '@astrojs/vercel';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tutechoahora.com',
   integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
